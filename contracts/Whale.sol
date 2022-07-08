ccb5413f115ecc797b461f6f9a5c426f1e2af023
alex t <aletapia@proton.me>
2022-07-07 19:09:39 -0600

    fixed contract

--- contracts/Whale.sol
+++ contracts/Whale.sol
@@ -6,124 +6,127 @@ address constant MARKETPLACE_ADDRESS = 0xD8f76d6C907E705E8C66D531f7C3386d33Dc2f2
 uint256 constant PERCENTAGE_MARKETPLACE_FEE = 10;
 
 contract WhaleStrategy {
-    address public immutable creatorWallet;
-    uint256 public whaleLimit;
-    uint256 public initialLairEntry;
+  address public immutable creatorWallet;
+  uint256 public whaleLimit;
+  uint256 public initialLairEntry;
 
-    bool public lairFull;
+  bool public lairFull;
 
-    Whale[] public whaleArr;
-    mapping(address => bool) public isWhale;
+  Whale[] public whaleArr;
+  mapping(address => bool) public isWhale;
 
-    struct Whale {
-        address addr;
-        uint256 grant;
+  struct Whale {
+    address addr;
+    uint256 grant;
+  }
+
+  event LogNewWhale(uint256 amount, address newWhale);
+  event LogDethroneWhale(uint256 amount, address newWhale, address oldWhale);
+
+  constructor(
+    address creatorAddress,
+    uint256 maxWhaleLimit,
+    uint256 initialCost
+  ) {
+    creatorWallet = creatorAddress;
+    whaleLimit = maxWhaleLimit;
+    initialLairEntry = initialCost;
+  }
+
+  function enterLair() public payable checkEtherGuard(msg.value) {
+    if (lairFull == false) {
+      _accomodateWhaleWithoutDethrone(msg.value, msg.sender);
+      // Calculate Mktplace fee
+      uint256 marketPlaceFee = _calculateAppFee(msg.value);
+      // Distribute ether
+      payable(MARKETPLACE_ADDRESS).transfer(marketPlaceFee);
+      payable(creatorWallet).transfer(msg.value - marketPlaceFee);
+      return;
     }
+    // lairFull == true
 
-    event LogNewWhale(uint256 amount, address newWhale);
-    event LogDethroneWhale(uint256 amount, address newWhale, address oldWhale);
+    Whale memory whaleToDethrone = whaleArr[whaleArr.length - 1];
+    _accomodateWhaleAndDethrone(msg.value, msg.sender);
+    // Refund old whale
+    payable(whaleToDethrone.addr).transfer(whaleToDethrone.grant);
+    // Distribute the profits
+    uint256 profit = msg.value - whaleToDethrone.grant;
+    uint256 appProfit = _calculateAppFee(profit);
+    payable(MARKETPLACE_ADDRESS).transfer(appProfit);
+    payable(creatorWallet).transfer(profit - appProfit);
+  }
 
-    constructor(
-        address creatorAddress,
-        uint256 maxWhaleLimit,
-        uint256 initialCost
-    ) {
-        creatorWallet = creatorAddress;
-        whaleLimit = maxWhaleLimit;
-        initialLairEntry = initialCost;
+  function _accomodateWhaleWithoutDethrone(uint256 moneyPaid, address newWhaleWallet) private {
+    // Include new whale in mapping and array
+    whaleArr.push(Whale(newWhaleWallet, moneyPaid));
+    isWhale[newWhaleWallet] = true;
+
+    emit LogNewWhale(moneyPaid, newWhaleWallet);
+
+    if (whaleArr.length >= whaleLimit) {
+      sort();
+      lairFull = true;
     }
+  }
 
-    function enterLair() public payable checkMoney(msg.value) {
-        if (lairFull == false) {
-            _accomodateWhaleWithoutDethrone(msg.value, msg.sender);
-            // Calculate Mktplace fee
-            uint256 marketPlaceFee = _calculateAppFee(msg.value);
-            // Distribute ether
-            payable(MARKETPLACE_ADDRESS).transfer(marketPlaceFee);
-            payable(creatorWallet).transfer(msg.value - marketPlaceFee);
-            return;
-        }
+  function _accomodateWhaleAndDethrone(uint256 newMoney, address newAddr) private {
+    Whale memory dethronedWhale = whaleArr[whaleArr.length - 1];
+    // Remove last element from array since it is sorted
+    whaleArr.pop();
+    // Remove whale from mapping
+    delete isWhale[dethronedWhale.addr];
+    emit LogDethroneWhale(newMoney, newAddr, dethronedWhale.addr);
+    // Include new whale in mapping and array
+    whaleArr.push(Whale(newAddr, newMoney));
+    isWhale[newAddr] = true;
+    // This step can be optimized by finding the
+    // pertaining location and moving the whales in
+    // the array
+    sort();
+  }
 
-        // LAIR FULL
-        Whale memory whaleToDethrone = whaleArr[whaleArr.length - 1];
-        _accomodateWhaleAndDethrone(msg.value, msg.sender);
-        // Refund old whale
-        payable(whaleToDethrone.addr).transfer(whaleToDethrone.grant);
+  function _calculateAppFee(uint256 amount) private pure returns (uint256) {
+    return (amount * PERCENTAGE_MARKETPLACE_FEE) / 100;
+  }
+
+  function sort() internal {
+    _quickSort(whaleArr, int256(0), int256(whaleArr.length - 1));
+  }
+
+  function _quickSort(
+    Whale[] storage arr,
+    int256 left,
+    int256 right
+  ) private {
+    int256 i = left;
+    int256 j = right;
+    if (i == j) return;
+    uint256 pivot = arr[uint256(left + (right - left) / 2)].grant;
+    while (i <= j) {
+      while (arr[uint256(i)].grant > pivot) i++;
+      while (pivot > arr[uint256(j)].grant) j--;
+      if (i <= j) {
+        Whale memory temp = arr[uint256(i)];
+        arr[uint256(i)] = arr[uint256(j)];
+        arr[uint256(j)] = temp;
+        i++;
+        j--;
+      }
     }
+    if (left < j) _quickSort(arr, left, j);
+    if (i < right) _quickSort(arr, i, right);
+  }
 
-    function _accomodateWhaleWithoutDethrone(
-        uint256 moneyPaid,
-        address newWhaleWallet
-    ) private {
-        whaleArr.push(Whale(newWhaleWallet, moneyPaid));
-        // Define whale in mapping
-        isWhale[newWhaleWallet] = true;
-        emit LogNewWhale(moneyPaid, newWhaleWallet);
-        if (whaleArr.length >= whaleLimit) {
-            sort();
-            lairFull = true;
-        }
+  modifier checkEtherGuard(uint256 amount) {
+    if (lairFull == false) {
+      require(amount >= initialLairEntry, 'Not enough money.');
+      _;
+      return;
     }
-
-    function _accomodateWhaleAndDethrone(uint256 newMoney, address newAddr)
-        private
-    {
-        Whale memory dethronedWhale = whaleArr[whaleArr.length - 1];
-        // Remove last element from array since it is sorted
-        whaleArr.pop();
-        // Remove whale from mapping
-        isWhale[dethronedWhale.addr] = false;
-        emit LogDethroneWhale(newMoney, newAddr, dethronedWhale.addr);
-        whaleArr.push(Whale(newAddr, newMoney));
-        sort();
+    if (lairFull == true) {
+      require(amount > whaleArr[whaleLimit - 1].grant, 'Not enough to dethrone whale.');
+      _;
+      return;
     }
-
-    function _calculateAppFee(uint256 amount) private pure returns (uint256) {
-        return (amount * PERCENTAGE_MARKETPLACE_FEE) / 100;
-    }
-
-    function sort() internal {
-        _quickSort(whaleArr, int256(0), int256(whaleArr.length - 1));
-    }
-
-    function _quickSort(
-        Whale[] storage arr,
-        int256 left,
-        int256 right
-    ) private {
-        int256 i = left;
-        int256 j = right;
-        if (i == j) return;
-        uint256 pivot = arr[uint256(left + (right - left) / 2)].grant;
-        while (i <= j) {
-            while (arr[uint256(i)].grant > pivot) i++;
-            while (pivot > arr[uint256(j)].grant) j--;
-            if (i <= j) {
-                (arr[uint256(i)], arr[uint256(j)]) = (
-                    arr[uint256(j)],
-                    arr[uint256(i)]
-                );
-                i++;
-                j--;
-            }
-        }
-        if (left < j) _quickSort(arr, left, j);
-        if (i < right) _quickSort(arr, i, right);
-    }
-
-    modifier checkMoney(uint256 amount) {
-        if (lairFull == false) {
-            require(amount >= initialLairEntry, "Not enough money.");
-            _;
-            return;
-        }
-        if (lairFull == true) {
-            require(
-                amount > whaleArr[whaleLimit - 1].grant,
-                "Not enough to dethrone whale."
-            );
-            _;
-            return;
-        }
-    }
+  }
 }
--- test/Whale.test.ts
+++ test/Whale.test.ts
@@ -1,25 +1,77 @@
-
 import { expect } from 'chai'
 import { ethers } from 'hardhat'
 import { Contract, BigNumber } from 'ethers'
 
-describe('Whale',  () => {
+describe('Whale', () => {
   let whale: Contract
 
-  beforeEach(async  () => {
+  beforeEach(async () => {
     const [acct1] = await ethers.getSigners()
     const Whale = await ethers.getContractFactory('WhaleStrategy')
-    whale = await Whale.deploy(acct1.address, 3, ethers.utils.parseEther("1"))
+    whale = await Whale.deploy(acct1.address, 3, ethers.utils.parseEther('1'))
     await whale.deployed()
   })
 
   it('lair fullness should be false after deploy', async () => {
-    expect(await whale.lairFull()).to.eq(false) 
+    expect(await whale.lairFull()).to.eq(false)
   })
-
-  it('should make a whale when entering lair with initial amount', async  () => {
-    const [,acct2] = await ethers.getSigners()
-    await whale.connect(acct2).enterLair({value: ethers.utils.parseEther("1")})
+  it('should revert when depositing less than the initial entry', async () => {
+    const [, acct2] = await ethers.getSigners()
+    expect(
+      whale.connect(acct2).enterLair({ value: ethers.utils.parseEther('0.9') })
+    ).to.be.revertedWith('Not enough money.')
+  })
+  it('should create a whale when entering lair initially', async () => {
+    const [, acct2] = await ethers.getSigners()
+    await whale.connect(acct2).enterLair({ value: ethers.utils.parseEther('1') })
     expect(await whale.isWhale(acct2.address)).to.eq(true)
+    const newWhale = await whale.whaleArr(0)
+    expect(newWhale.addr).to.equal(acct2.address)
   })
+  it('lair should be filled when max whales enter', async () => {
+    const [, acct2, acct3, acct4] = await ethers.getSigners()
+    await whale.connect(acct2).enterLair({ value: ethers.utils.parseEther('1') })
+    await whale.connect(acct3).enterLair({ value: ethers.utils.parseEther('2') })
+    await whale.connect(acct4).enterLair({ value: ethers.utils.parseEther('3') })
+    expect(await whale.lairFull()).to.be.true
+  })
+  it('lair should get sorted after lair gets full', async () => {
+    const [, acct2, acct3, acct4] = await ethers.getSigners()
+    await whale.connect(acct2).enterLair({ value: ethers.utils.parseEther('1') })
+    await whale.connect(acct3).enterLair({ value: ethers.utils.parseEther('2') })
+    await whale.connect(acct4).enterLair({ value: ethers.utils.parseEther('3') })
+    const biggestWhale = await whale.whaleArr(0)
+    const smallestWhale = await whale.whaleArr(2)
+    expect(biggestWhale.addr).to.equal(acct4.address)
+    expect(smallestWhale.addr).to.equal(acct2.address)
+  })
+  it('lowest whale should be replaced by new whale when lair is full', async () => {
+    const [, acct2, acct3, acct4, acct5] = await ethers.getSigners()
+    await whale.connect(acct2).enterLair({ value: ethers.utils.parseEther('1') })
+    await whale.connect(acct3).enterLair({ value: ethers.utils.parseEther('2') })
+    await whale.connect(acct4).enterLair({ value: ethers.utils.parseEther('3') })
+    await whale.connect(acct5).enterLair({ value: ethers.utils.parseEther('4') })
+    expect((await whale.whaleArr(2)).addr).to.equal(acct3.address)
+    expect(await whale.isWhale(acct2.address)).to.be.false
+    expect(await whale.isWhale(acct5.address)).to.be.true
+  })
+  it('should revert when depositing less than lowest whale', async () => {
+    const [, acct2, acct3, acct4, acct5] = await ethers.getSigners()
+    await whale.connect(acct2).enterLair({ value: ethers.utils.parseEther('2') })
+    await whale.connect(acct3).enterLair({ value: ethers.utils.parseEther('3') })
+    await whale.connect(acct4).enterLair({ value: ethers.utils.parseEther('4') })
+    expect(
+      whale.connect(acct5).enterLair({ value: ethers.utils.parseEther('1.5') })
+    ).to.be.revertedWith('Not enough to dethrone whale.')
+  })
+  it('should transfer ether correctly to creator when lair not full', async () => {
+    const [acct1, acct2, acct3, acct4] = await ethers.getSigners()
+    const oldBalance = await acct1.getBalance()
+    await whale.connect(acct2).enterLair({ value: ethers.utils.parseEther('1') })
+    await whale.connect(acct3).enterLair({ value: ethers.utils.parseEther('2') })
+    await whale.connect(acct4).enterLair({ value: ethers.utils.parseEther('3') })
+    const newBalance = await acct1.getBalance()
+    expect(newBalance.sub(oldBalance)).to.equal(ethers.utils.parseEther('5.40'))
+  })
+  it('should transfer correctly when dethrone occurs', async () => {})
 })
